31cdc52c113932d281caae71ee982fd5ba35fd6e
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-02-08 06:56:39 +0100

    collection sorting and cache refreshing

---
+++ app/Models/Post.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+use function PHPUnit\Framework\throwException;
+
+class Post{
+
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug){
+
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+
+    }
+
+    public static function find($slug){
+        
+        // if (!file_exists($path =  resource_path("posts/{$slug}.html"))) {
+
+        //     throw new ModelNotFoundException();
+        // }
+
+        // return  cache()->remember("posts.{$slug}", now()->addMinutes(20), fn() => file_get_contents($path));
+        
+        //############# Have been done in a better way #############
+        // of all the blog post find the one with a slug that matches the one that was requested
+        return static::all()->firstWhere('slug', $slug);
+
+        
+
+    }
+
+    public static function all(){
+        $files = File::files(resource_path("posts"));
+
+    //    return array_map(function($file){
+    //         return $file->getContents();
+    //     }, $files);
+
+       // return array_map(fn($file) =>  $file->getContents(), $files);
+
+       //#############Have been done in a better way#############
+      return collect($files)
+     ->map(fn($file) =>  YamlFrontMatter::parseFile($file))
+     ->map(function($document){
+       
+        return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+     });
+    }
+}
