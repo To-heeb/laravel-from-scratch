979ecc5a04b434bb09adfbb96d440772d61c26ef
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-02-11 02:16:31 +0100

    route-model binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,74 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-use function PHPUnit\Framework\throwException;
+class Post extends Model
+{
+    use HasFactory;
 
-class Post{
+    protected $fillable = ['title', 'excerpt', 'body'];
 
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug){
-
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-
-    }
-
-    public static function find($slug){
-        
-        // if (!file_exists($path =  resource_path("posts/{$slug}.html"))) {
-
-        //     throw new ModelNotFoundException();
-        // }
-
-        // return  cache()->remember("posts.{$slug}", now()->addMinutes(20), fn() => file_get_contents($path));
-        
-        //############# Have been done in a better way #############
-        // of all the blog post find the one with a slug that matches the one that was requested
-        return static::all()->firstWhere('slug', $slug);
-
-        
-
-    }
-
-    public static function findOrFail($slug){
-        $post = static::find($slug);
-
-        if (!$post) {
-            throw new ModelNotFoundException();
-        };
-
-    }
-
-    public static function all(){
-        $files = File::files(resource_path("posts"));
-
-    //    return array_map(function($file){
-    //         return $file->getContents();
-    //     }, $files);
-
-       // return array_map(fn($file) =>  $file->getContents(), $files);
-
-       //#############Have been done in a better way#############
-       return cache()->rememberForever('posts.all', function () {
-        return collect(File::files(resource_path("posts")))
-        ->map(fn($file) =>  YamlFrontMatter::parseFile($file))
-        ->map(function($document){
-          
-           return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-        })
-        ->sortByDesc('date');
-       });
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,82 +16,17 @@ use Illuminate\Support\Facades\File;
 |
 */
 
-Route::get('/', function () {
-    
-
-    $files = File::files(resource_path("posts"));
-
-    // $posts = [];
-
-    // foreach ($files as $file) {
-        
-    //     $document = YamlFrontMatter::parseFile($file);
-
-    //     $posts[] = new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-    // }
-
-    //<###############----This is a better way of doing what is above using array_map---#################>
-    // $posts = array_map(function($file){
-    //     $document = YamlFrontMatter::parseFile($file);
-    //     return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-
-    // }, $files);
-
-     //<###############----This is a simila way of doing what is above using laravel collection with map---#################>
-    //  $posts = collect($files)
-    //  ->map(fn($file) =>  YamlFrontMatter::parseFile($file))
-    //  ->map(function($document){
-       
-    //     return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-    //  });
-
-    //  ddd($posts);
-
-    // $document = YamlFrontMatter::parseFile(
-    //     resource_path('posts/myfourthpost.html')
-    // );
-
-    // ddd($document->date);
-
-    //ddd((string)$posts[0]);
-    //ddd($posts[0]->getContents);
-    //ddd($posts);
-    
-    //return view('post', ['posts' => $posts]);
+Route::get('/', function () {    
 
     return view('post', ['posts'=> Post::all()]);
 });
 
-Route::get('posts/{post}', function($slug){
+Route::get('posts/{post:slug}', function(Post $post){ //laravel does Post::where('slug', $post)->firstOrFail()
 
 //Find a post by its slug and pass it to a view called "post"
-$post = Post::findOrFail($slug);
+//$post = Post::findOrFail($post);
 //ddd($post);
 return view('posts',['post'=>$post]);
 
 
-//     $path = __DIR__ ."/../resources/posts/{$slug}.html";
-
-//     if (!file_exists($path)) {
-
-//         //ddd('File does not exist');
-//         //dd('File does not exist');
-//         //abort(404);
-//         //return $path;
-//        return redirect('/');
-//     }
-
-// //    $post =  cache()->remember("posts.{$slug}", now()->addMinutes(20), function() use ($path){
-// //        var_dump('file-get_contents');
-// //         return file_get_contents($path);
-// //     });
-
-//     //php arrow function
-//     $post =  cache()->remember("posts.{$slug}", now()->addMinutes(20), fn() => file_get_contents($path));
-
-//     //$post = file_get_contents($path);
-
-//     return view('posts', ['post' => $post] );
 });
-
-//->where('post', '[A-z_/-]+');
